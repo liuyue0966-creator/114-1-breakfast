f7b7fd9527a3045506da89bdffbe869c5fec02cf
liuyue0966 <liuyue0966@gmail.com>
2025-10-18 00:37:28 +0800

    feat: 新增目前上課內容

--- 114-1-breakfast-master/src/App.jsx
+++ 114-1-breakfast-master/src/App.jsx
@@ -8,8 +8,8 @@ export default function App() {
     <div className="min-h-screen flex flex-col bg-base-100">
       <Header />
       <main className="flex-1 container mx-auto px-4 py-8">
-        <Outlet />
-      </main>
+        <Outlet />{""}
+      </main>{""}
       <Footer />
     </div>
   );
