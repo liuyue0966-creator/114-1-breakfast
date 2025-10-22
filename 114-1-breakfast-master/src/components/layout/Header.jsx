dab92684ed14eac71f52f156f5a3f4a90e6848b6
DESKTOP-F0GH6SG\USER <DESKTOP-F0GH6SG\USER>
2025-10-22 17:59:16 +0800

    feat: 新增第六周進度

--- 114-1-breakfast-master/src/components/layout/Header.jsx
+++ 114-1-breakfast-master/src/components/layout/Header.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
-import { HiBars3 } from "react-icons/hi2";
+import { HiBars3 , HiMoon, HiSun} from "react-icons/hi2";
 import { Link, useLocation } from "react-router-dom";
 import NavLinks  from "./NavLinks";
 import { NAV_ITEMS, isPathActive } from "../../utils/navigation";
+import UserMenu from "./UserMenu";
+import useTheme from "../../hooks/useTheme.js"
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { theme, toggleTheme } = useTheme("dark");
   const location = useLocation();
   const isNavItemActiv = (path) => isPathActive(path, location.pathname);
   return (
@@ -32,6 +35,7 @@ export default function Header() {
               }
             />
           )}
+        </div>
           {/* 導覽列的中間部分:Logo品牌 */}
           <Link 
             to="/"
@@ -48,7 +52,23 @@ export default function Header() {
               listClassName="menu menu-horizontal px-1"
             />
           </div>
-        </div>
+          {/* 導覽列右邊部分:切換主題、使用者設定 */}
+          <div className="navbar-end flex items-center gap-2">
+            {/* 主題切換按鈕 */}
+            <button
+              onClick={toggleTheme}
+              aria-label="切換主題"
+              className="btn btn-ghost btn-circle border-2 bg-base-300"
+            >
+              {theme === "dark" ? (
+                <HiMoon className="w-6 h-6" />
+              ) : (
+                <HiSun className="w-6 h-6" />
+              )}
+            </button>
+            {/* 使用者設定: 下拉式選單 */}
+            <UserMenu />
+          </div>
       </header>
     );
 }
